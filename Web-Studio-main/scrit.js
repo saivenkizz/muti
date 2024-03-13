0ba1c094340913cf7d58b85ba2c0b87f19856537
saivenkizz <122435654+saivenkizz@users.noreply.github.com>
2024-03-13 19:01:26 +0530

    Create scrit.js

---
+++ Web-Studio-main/scrit.js
@@ -0,0 +1,5 @@
+document.getElementById('logoutButton').addEventListener('click', function() {
+    alert('Logging out!');
+    // Implement the logout functionality here.
+    // For demonstration, it just shows an alert.
+});
